8865150f85280c32fab10ad6908fcc4f1e31e116
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2023-11-16 01:35:16 +0300

    feat: add bakery page

--- src/utils/script.js
+++ src/utils/script.js
@@ -1,12 +1,16 @@
 document.getElementById('logo').addEventListener('click', () => {
-    window.location.href = '../main/main.html';
+  window.location.href = '../main/main.html';
 });
 
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+
+setTimeout(() => {
+  document.getElementById('delivery').style.transform = 'translate(0)';
+}, 1000)
